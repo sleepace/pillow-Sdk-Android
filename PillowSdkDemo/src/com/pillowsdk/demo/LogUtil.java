173ecc2a472ec56fc8d03077ec36fe30f889fee0
jaker <jaker@sleepace.net>
2017-05-17 16:53:10 +0800

    first commit

---
+++ PillowSdkDemo/src/com/pillowsdk/demo/LogUtil.java
@@ -0,0 +1,13 @@
+package com.pillowsdk.demo;
+
+import android.util.Log;
+
+
+public class LogUtil {
+
+	private static final String TAG = "PillowSdk";
+	
+	public static void showMsg(String msg){
+		Log.i(TAG, msg);
+	}
+}
